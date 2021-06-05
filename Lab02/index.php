6aa0ddd24106fe0a88c0472f3b617497256a8a4d
namphuong01998 <72931040+namphuong01998@users.noreply.github.com>
2021-06-05 21:09:14 +0700

    Add files via upload

---
+++ Lab02/index.php
@@ -0,0 +1,20 @@
+<?php 
+    include "src/header.php";
+?>
+
+<div class="fb-header">
+    <div id="img1" class="fb-header"><img src="img/facebook.png"/></div>
+    <?php
+        include "form_login.php";
+    ?>
+</div>
+
+<div class="fb-body">
+<?php 
+    include "form_register.php";
+?>
+</div>
+
+<?php
+    include "src/footer.php";
+?>
---
+++ Lab02/src/form_login.php
@@ -0,0 +1,13 @@
+<form method="POST" action="">
+    <div id="form1" class="fb-header">Email or Phonr<br>
+        <input placeholder="Email" type="mail" name="name"/><br>
+        <input type="checkbox"/>Keep me logged in
+    </div>
+
+    <div id="form2" class="fb-header">Password<br>
+        <input placeholder="Password" type="password" name="password"/><br>
+        Forgotten your password?
+    </div>
+
+    <input type="submit" name="login" class="submit1" value="login"/>
+</form>
