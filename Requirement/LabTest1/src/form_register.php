cd9cbed318d62e7f26f90d2b7c71ee6207b047c2
namphuong01998 <72931040+namphuong01998@users.noreply.github.com>
2021-06-09 12:24:22 +0700

    Add files via upload

---
+++ Requirement/LabTest1/src/form_register.php
@@ -0,0 +1,26 @@
+<div id="intro1" class="fb-body">
+    Facebook helps you connect and share with the<br>
+    people in your life
+</div>
+<div id="img2" class="fb-body"><img src="img/world.png"/></div>
+
+<div id="intro2" class="fb-body">Create an account</div>
+<div id="intro3" class="fb-body">It's free and always will be.</div>
+
+<form method="POST" action="account_infor.php">
+	<div id="form3" class="fb-body">
+		<input placeholder="First Name" type="text" id="namebox" name="firstname" />
+		<input placeholder="Last Name" type="text" id="namebox"  name="lastname"/> <br>
+		<input placeholder="Emai" type="text" id="mailbox" name="mail"/><br>
+		<input placeholder="Re-enter email" type="text" id="mailbox"  /><br>
+		<input placeholder="Password" type="password" id="mailbox" name="pass" /><br>
+		<input type="date" id="namebox" name="birthday"/><br><br>
+		<input type="radio" id="r-b" name="sex" value="male" />Male
+		<input type="radio" id="r-b" name="sex" value="female" />Female<br><br>
+		<p id="intro4">By clicking Create an account, you agree to our Terms and that 
+					you have read our Data Policy, including our Cookie Use.</p>
+		<input type="submit" class="button2" value="Create an account" />
+		<br><hr>
+		<p id="intro5">Create a Page for a celebrity, band or business.</p>
+	</div>
+</form>
