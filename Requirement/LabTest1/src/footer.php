395b38c91c6b6d3655754f84ff6df606b7cadd30
namphuong01998 <72931040+namphuong01998@users.noreply.github.com>
2021-06-09 12:23:52 +0700

    Delete Requirement/LabTest1 directory

--- Requirement/LabTest1/src/footer.php
+++
@@ -1,8 +0,0 @@
-<div class="fb-body-footer">
-				<div id="fb-body-footer-base" class="fb-body-footer">English (UK)<br><hr>
-				Sign Up	Log In	&copy; www.coderglass.com &nbsp;&nbsp; Design by Varun Singh
-				</div>
-		</div>
-	</body>
-	
-</html>
--- Requirement/LabTest1/src/form_confirm.php
+++
@@ -1,4 +0,0 @@
-<form method="POST" action="welcome.php">
-    <input type="submit" class="button2" name="button_confirm" value="Confirm"/>
-</form>
-
